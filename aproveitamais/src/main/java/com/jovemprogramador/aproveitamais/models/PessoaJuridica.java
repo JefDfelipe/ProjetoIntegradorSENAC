27f79f20b2a7e6cacd8b3004cdc546cf7f86102b
LucasdaSilvaSantos <ldsantos2004@gmail.com>
2023-09-25 14:56:03 -0300

    fix: "n sei o q aconteceu mas agr foi"

--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/PessoaJuridica.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/PessoaJuridica.java
@@ -48,36 +48,43 @@ public class PessoaJuridica implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        
         throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
     }
 
     @Override
     public String getPassword() {
+        
         return this.senha;
     }
 
     @Override
     public String getUsername() {
+        
         return this.login;
     }
 
     @Override
     public boolean isAccountNonExpired() {
+        
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
+        
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
+        
         return true;    
     }
 
     @Override
     public boolean isEnabled() {
+        
         return true;
     }
 
