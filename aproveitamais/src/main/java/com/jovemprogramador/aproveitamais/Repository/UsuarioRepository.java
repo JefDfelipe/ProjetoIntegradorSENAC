a656151acce5c2e78fda75e3ac2f5fa2384044d0
JoZon <joaojminecraftabreu@gmail.com>
2023-09-25 14:07:30 -0300

    fix: "UsuarioRepository arrumado"

--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/Repository/UsuarioRepository.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/Repository/UsuarioRepository.java
@@ -3,12 +3,13 @@ package com.jovemprogramador.aproveitamais.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jovemprogramador.aproveitamais.models.PessoaJuridica;
+import com.jovemprogramador.aproveitamais.models.UsuarioCliente;
 
 public interface UsuarioRepository extends JpaRepository<PessoaJuridica, String> {
     
 
-        PessoaJuridica findByLogin(String login);
+        PessoaJuridica findByLoginPJ(String login);
 
-        UsuarioCliente findByLogin(String login);
+        UsuarioCliente findByLoginPF(String login);
 
 }
