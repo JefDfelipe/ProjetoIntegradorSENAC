854c97e96b0c798d249d22c53073041c6c9d380f
JoZon <joaojminecraftabreu@gmail.com>
2023-09-25 14:50:03 -0300

    fix: "TODO auto-generated method stub"

--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/UsuarioCliente.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/UsuarioCliente.java
@@ -147,43 +147,36 @@ public class UsuarioCliente implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
     }
 
     @Override
     public String getPassword() {
-        // TODO Auto-generated method stub
         return this.getPassword();
     }
 
     @Override
     public String getUsername() {
-        // TODO Auto-generated method stub
-        return this.getUsername();
+		        return this.getUsername();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
         return true;
     }
      
