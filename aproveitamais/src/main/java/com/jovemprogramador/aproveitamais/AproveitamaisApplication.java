810cc79ddb64f23764378b23ad87722760cbbd9c
JoZon <joaojminecraftabreu@gmail.com>
2023-09-29 15:03:05 -0300

    fix: ""

--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/AproveitamaisApplication.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/AproveitamaisApplication.java
@@ -2,10 +2,6 @@ package com.jovemprogramador.aproveitamais;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
 
 @SpringBootApplication
 public class AproveitamaisApplication {
--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/PessoaFisica.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/PessoaFisica.java
@@ -11,8 +11,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.util.UUID;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.validation.constraints.NotBlank;
 
 
