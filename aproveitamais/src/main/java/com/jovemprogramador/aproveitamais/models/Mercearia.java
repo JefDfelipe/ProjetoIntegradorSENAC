da99bc9ec049784058c85efac6c002a2e8e78726
LucasdaSilvaSantos <ldsantos2004@gmail.com>
2023-10-02 14:40:48 -0300

    feat: "Outra categoria adicionada"

---
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/Mercearia.java
@@ -0,0 +1,26 @@
+package com.jovemprogramador.aproveitamais.models;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Mercearia extends Produtos {
+    
+    private static final long serialVersionUID = 1L;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID merceariaId;
+
+    @NotBlank
+    @Column(nullable = false, unique = false)
+    private String subCategoriaM;
+
+}
