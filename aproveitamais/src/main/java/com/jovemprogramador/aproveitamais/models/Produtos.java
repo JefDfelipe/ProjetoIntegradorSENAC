d6bd71b2f7ff721872fd7092fa253bb303d64dc4
JefDfelipe <fejedamas@hotmail.com>
2023-10-20 14:56:11 -0300

    refactor: "ident"

--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/Produtos.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/Produtos.java
@@ -14,80 +14,80 @@ import jakarta.validation.constraints.NotBlank;
 @Entity
 public class Produtos {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO) 
-    private UUID produtoId;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID produtoId;
 
-    @NotBlank
-    @Column(nullable = false, unique = true)
-    private long codigoDeBarras;
+  @NotBlank
+  @Column(nullable = false, unique = true)
+  private long codigoDeBarras;
 
-    @NotBlank
-    @Column(nullable = false, unique = false)
-    private String marca;
+  @NotBlank
+  @Column(nullable = false, unique = false)
+  private String marca;
 
-    @NotBlank
-    @Column(nullable = false, unique = true)
-    private String categoria;
+  @NotBlank
+  @Column(nullable = false, unique = true)
+  private String categoria;
 
-    @NotBlank
-    @Column(nullable = false, unique = false)
-    private String mercadoDeOrigem;
+  @NotBlank
+  @Column(nullable = false, unique = false)
+  private String mercadoDeOrigem;
 
-    @NotBlank
-    @Column(nullable = false, unique = false)
-    private float preco;
+  @NotBlank
+  @Column(nullable = false, unique = false)
+  private float preco;
 
-  //----------------------------------------------------------------------\\
+  // ----------------------------------------------------------------------\\
 
-    public UUID getProdutoId() {
-        return produtoId;
-    }
+  public UUID getProdutoId() {
+    return produtoId;
+  }
 
-    public void setProdutoId(UUID produtoId) {
-        this.produtoId = produtoId;
-    }
+  public void setProdutoId(UUID produtoId) {
+    this.produtoId = produtoId;
+  }
 
-    public long getCodigoDeBarras() {
-        return codigoDeBarras;
-    }
+  public long getCodigoDeBarras() {
+    return codigoDeBarras;
+  }
 
-    public void setCodigoDeBarras(long codigoDeBarras) {
-        this.codigoDeBarras = codigoDeBarras;
-    }
+  public void setCodigoDeBarras(long codigoDeBarras) {
+    this.codigoDeBarras = codigoDeBarras;
+  }
 
-    public String getMarca() {
-        return marca;
-    }
+  public String getMarca() {
+    return marca;
+  }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
+  public void setMarca(String marca) {
+    this.marca = marca;
+  }
 
-    public String getCategoria() {
-        return categoria;
-    }
+  public String getCategoria() {
+    return categoria;
+  }
 
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
-    }
+  public void setCategoria(String categoria) {
+    this.categoria = categoria;
+  }
 
-    public String getMercadoDeOrigem() {
-        return mercadoDeOrigem;
-    }
+  public String getMercadoDeOrigem() {
+    return mercadoDeOrigem;
+  }
 
-    public void setMercadoDeOrigem(String mercadoDeOrigem) {
-        this.mercadoDeOrigem = mercadoDeOrigem;
-    }
+  public void setMercadoDeOrigem(String mercadoDeOrigem) {
+    this.mercadoDeOrigem = mercadoDeOrigem;
+  }
 
-    public float getPreco() {
-        return preco;
-    }
+  public float getPreco() {
+    return preco;
+  }
 
-    public void setPreco(float preco) {
-        this.preco = preco;
-    }
+  public void setPreco(float preco) {
+    this.preco = preco;
+  }
 
 }
