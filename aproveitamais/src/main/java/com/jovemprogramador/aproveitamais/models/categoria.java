6eaa29f84092dd51bb62e561015341e087d89c79
JoZonnoZoJ <joaojminecraftabreu@gmail.com>
2023-10-23 14:42:50 -0300

    feat: "enum categoria"

---
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/categoria.java
@@ -0,0 +1,16 @@
+package com.jovemprogramador.aproveitamais.models;
+
+public enum categoria {
+
+    BEBIDAS_ALCOOLICAS,
+    BEBIDAS,
+    MATINAIS,
+    LATICINIOS,
+    CARNES,
+    PADARIA,
+    FRUTAS,
+    VERDURAS,
+    CONGELADOS,
+
+    
+}
