0373eb23af9c3e5e95be9025ef8660bdb0e0bf2a
JoZonnoZoJ <joaojminecraftabreu@gmail.com>
2023-10-23 14:47:41 -0300

    feat: "addon"

--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/Produtos.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/models/Produtos.java
@@ -5,6 +5,8 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -30,6 +32,7 @@ public class Produtos {
 
   @NotBlank
   @Column(nullable = false, unique = true)
+  @Enumerated(EnumType.STRING)
   private String categoria;
 
   @NotBlank
