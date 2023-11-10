2f2f9c71574f5f2798af129b45def6ece996c716
JefDfelipe <fejedamas@hotmail.com>
2023-11-10 16:17:09 -0300

    feat: "implementa websecuritycustomizer para ignorar arquivos de estilo"

--- aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/Security/WebSecurityConfig.java
+++ aproveitamais/src/main/java/com/jovemprogramador/aproveitamais/Security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
@@ -36,6 +37,11 @@ public class WebSecurityConfig {
             .permitAll());
 
     return http.build();
+  }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().requestMatchers("/fonts/**", "/images/**", "/js/**", "/scss/**", "/style/**");
   }
 
 }
